627a9bea8199e95dc9efaf9c8851752f3feaac28
Will Jarvis <WillJarvis@Wills-MacBook-Pro-452.local>
2024-08-21 08:25:31 -0400

    initial commit

---
+++ src/main/java/bfan/backend/production/ProductionApplication.java
@@ -0,0 +1,13 @@
+package bfan.backend.production;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bfan/backend/production/ProductionApplicationTests.java
@@ -0,0 +1,13 @@
+package bfan.backend.production;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
